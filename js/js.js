6088adeb248ead7f8a8a4510522784522af4945c
JuanAntonioBieto <32370471+JuanAntonioBieto@users.noreply.github.com>
2018-05-13 13:11:14 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,36 @@
+﻿var cargado = 1;
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height()) {
+        cargar();
+	}
+});
+
+function cargar() {
+    if (cargado < 3) {
+        $.getJSON("https://rawgit.com/JuanAntonioBieto/Pagina-de-noticias/master/json/" + cargado + ".json", function (jsonObject) {
+            addrow(jsonObject);
+		}); cargado++;
+		} else {
+        $('#mas').text('No hay más noticias');
+	}
+};
+
+function addrow(json) {
+    $.each(json, function (i, item) {
+        $("#contenedor5").append(
+		'<div class="container-fluid noticias">' +
+		'<div class="row" >' +
+		'<div class="col-sm-4">' + 
+		'<img class="img-responsive imagenes" src="' + item.imgbig +'"alt="imagen">' +
+		'</div>' +
+		'<div class="col-sm-8">' +
+		'<p class="titulos text-justify">' + item.title + '</p>' +
+		'<p class="textos text-justify">' + item.description + '</p>' +
+		'<p class="fechas">' + item.datetime + '</p>' +
+		'</div>' +
+		'</div>' +
+		'</div>');
+	})
+};
